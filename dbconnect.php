327f2520f4e62640a0305940c10bde3772a6db9f
vshelke <vaibhavshelke017@gmail.com>
2017-08-10 16:20:35 +0530

    Update: files added

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_SERVER", "mysql.hostinger.in");
-define("DB_USER", "u151434359_root");
-define("DB_PASSWORD", "vbhv3301");
-define("DB_DATABASE", "u151434359_cram");
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "cram");
 $conn = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
  if (!$conn) {
      die("Connection failed: " . mysqli_connect_error());
